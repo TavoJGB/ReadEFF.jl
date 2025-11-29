a0f5e6f8e0d6c1510b1d700c661f39d7f9761097
TavoJGB <116122159+TavoJGB@users.noreply.github.com>
2025-11-30 00:10:18 +0100

    update in DataReader

--- run.jl
+++ run.jl
@@ -5,4 +5,4 @@ datadir = joinpath(pwd(), "..", "IWTdata", "data", "eff")
 identifier_ranges = (:year => [2002:3:2020;2022], :imputation => 1:5)
 
 # Read EFF data
-eff_ii, eff_hh = read_eff(datadir, identifier_ranges; ifile_name="eff_vars_ii.csv", hfile_name="eff_vars_hh.csv")
+eff_ii, eff_hh = read_eff(datadir, identifier_ranges; i_list_filename="eff_vars_ii.csv", h_list_filename="eff_vars_hh.csv")
